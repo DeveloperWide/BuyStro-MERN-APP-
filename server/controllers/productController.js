a45bff4a323e411fae0ca440d3d2daa10274db9e
Mahesh Rana <maheshisdelta@gmail.com>
2025-10-26 00:04:09 +0530

    Create delete, update routes for product

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,47 +1,118 @@
+import mongoose from "mongoose";
 import Product from "../models/Product.js";
 
-
 export const getAllProducts = async (req, res) => {
-  const allProducts = await Product.find();
-  res.status(200).json({
-    success: true,
-    data: allProducts,
-  });
-}
+  try {
+    const allProducts = await Product.find();
+    res.status(200).json({
+      success: true,
+      data: allProducts,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Internal Server ERROR",
+    });
+  }
+};
 
 export const createProduct = async (req, res) => {
-    let {title, description, images, quantity, price} = req.body;
+  try {
+    let { title, description, images, quantity, price } = req.body;
 
-    if(!title || !description || !quantity || !price){
-        return res.status(400).json({
-            success: false,
-            message: "All Fields are required"
-        })
+    if (!title || !description || !quantity || !price) {
+      return res.status(400).json({
+        success: false,
+        message: "All Fields are required",
+      });
     }
 
-    if(!images){
-        return res.status(400).json({
-            success: false,
-            message: "Please Provide a image for Product"
-        })
+    if (!images) {
+      return res.status(400).json({
+        success: false,
+        message: "Please Provide a image for Product",
+      });
     }
 
     const newProduct = new Product({
-        title, description, images, quantity, price
+      title,
+      description,
+      images,
+      quantity,
+      price,
     });
     const svdProduct = await newProduct.save();
 
-    console.log(svdProduct);
+    return res.status(201).json({
+      success: true,
+      message: "Product Successfully created",
+      svdProduct,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server ERROR",
+    });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  try {
+    let { id } = req.params;
+
+    const productToBeUpdated = await Product.findById(id);
+    console.log("Product TO Be Updated : ", productToBeUpdated);
+
+    if (!productToBeUpdated) {
+      return res.status(404).json({
+        success: false,
+        message: "404: Product NOT Found!",
+      });
+    }
+
+    const updatedProduct = await Product.findByIdAndUpdate(
+      id,
+      {
+        ...req.body,
+      },
+      { new: true }
+    );
+
+    console.log("Updated Product : ", updatedProduct);
 
     return res.status(201).json({
-        success: true,
-        message: "Product Successfully created",
-        product: {
-            title: svdProduct.title,
-            description: svdProduct.description,
-            images: svdProduct.images,
-            quantity: svdProduct.quantity,
-            price: svdProduct.price
-        }
+      success: true,
+      message: "Product Updated Successfully",
+      updatedProduct,
     });
-}
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server ERROR",
+    });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+
+    if (!deletedProduct) {
+      return res.status(404).json({
+        success: false,
+        message: "NO Product Found!",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Product Deleted Successfully",
+      deletedProduct,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server ERROR",
+    });
+  }
+};
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { createProduct, getAllProducts } from "../controllers/productController.js";
+import { createProduct, deleteProduct, getAllProducts, updateProduct } from "../controllers/productController.js";
 const router = express.Router();
 
 router.get("/all", getAllProducts);
-router.post('/create', createProduct)
+router.post('/create', createProduct);
+router.put('/update/:id', updateProduct);
+router.delete('/delete/:id', deleteProduct);
 
 export default router;
